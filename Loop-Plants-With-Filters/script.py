9a87d8f4a6f03e7e7eb444877142eb638c997104
rdegosse <35624447+rdegosse@users.noreply.github.com>
2018-03-06 20:13:34 +0100

    debug

--- Loop-Plants-With-Filters/script.py
+++ Loop-Plants-With-Filters/script.py
@@ -12,9 +12,12 @@ if __name__ == "__main__":
 
     log('Start...', message_type='info', title=FARMWARE_NAME)
     
-    #reload(sys)
-    #sys.setdefaultencoding('utf8') #force utf8 for celerypy return code
-    
+    try:
+        reload(sys)
+        sys.setdefaultencoding('utf8') #force utf8 for celerypy return code
+    except:
+        pass
+
     try:
         farmware = MyFarmware(FARMWARE_NAME)
     except Exception as e:
@@ -22,12 +25,11 @@ if __name__ == "__main__":
         raise Exception(e)
     else:
         try:
-            #farmware.run()
-            pass
+            farmware.run()
         except Exception as e:
             log(e ,message_type='error', title=FARMWARE_NAME + " : run" )
             raise Exception(e)
-    farmware.run()
+
 
     log('End...', message_type='info', title=FARMWARE_NAME)
 
