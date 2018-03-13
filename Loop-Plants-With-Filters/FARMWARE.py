d70bc6f316cdafa7de76471d9f16718cb075e6fb
rdegosse <35624447+rdegosse@users.noreply.github.com>
2018-03-13 20:42:38 +0100

    debug

--- Loop-Plants-With-Filters/FARMWARE.py
+++ Loop-Plants-With-Filters/FARMWARE.py
@@ -19,12 +19,18 @@ class MyFarmware():
         self.input_filter_meta_key = os.environ.get(prefix+"_filter_meta_key", 'None')
         self.input_filter_meta_op = os.environ.get(prefix+"_filter_meta_op", 'None')
         self.input_filter_meta_value = os.environ.get(prefix+"_filter_meta_value", 'None')
+        self.input_filter_min_x = os.environ.get(prefix+"_filter_min_x", 'None')
+        self.input_filter_max_x = os.environ.get(prefix+"_filter_max_x", 'None')
+        self.input_filter_min_y = os.environ.get(prefix+"_filter_min_y", 'None')
+        self.input_filter_max_y = os.environ.get(prefix+"_filter_max_y", 'None')
         self.input_sequence_init = os.environ.get(prefix+"_sequence_init", 'None').split(",")
         self.input_sequence_beforemove  = os.environ.get(prefix+"_sequence_beforemove", 'None').split(",")
         self.input_sequence_aftermove = os.environ.get(prefix+"_sequence_aftermove", 'None').split(",")
         self.input_sequence_end = os.environ.get(prefix+"_sequence_end", 'None').split(",")
         self.input_save_meta_key = os.environ.get(prefix+"_save_meta_key", 'None')
         self.input_save_meta_value = os.environ.get(prefix+"_save_meta_value", 'None')
+        self.input_offset_x = int(os.environ.get(prefix+"_offset_x", 0))
+        self.input_offset_y = int(os.environ.get(prefix+"_offset_y", 0))
         self.input_default_z = int(os.environ.get(prefix+"_default_z", 0))
         self.input_default_speed = int(os.environ.get(prefix+"_default_speed", 800))
         self.input_debug = int(os.environ.get(prefix+"_debug", 2))
@@ -37,12 +43,18 @@ class MyFarmware():
             log('age_max_day: {}'.format(self.input_age_max_day), message_type='debug', title=self.farmwarename)
             log('filter_meta_key: {}'.format(self.input_filter_meta_key), message_type='debug', title=self.farmwarename)
             log('filter_meta_value: {}'.format(self.input_filter_meta_value), message_type='debug', title=self.farmwarename)
+            log('filter_min_x: {}'.format(self.input_filter_min_x), message_type='debug', title=self.farmwarename)
+            log('filter_max_x: {}'.format(self.input_filter_max_x), message_type='debug', title=self.farmwarename)
+            log('filter_min_y: {}'.format(self.input_filter_min_y), message_type='debug', title=self.farmwarename)
+            log('filter_max_y: {}'.format(self.input_filter_max_y), message_type='debug', title=self.farmwarename)
             log('sequence_init: {}'.format(self.input_sequence_init), message_type='debug', title=self.farmwarename)
             log('sequence_beforemove: {}'.format(self.input_sequence_beforemove), message_type='debug', title=self.farmwarename)
             log('sequence_aftermove: {}'.format(self.input_sequence_aftermove), message_type='debug', title=self.farmwarename)
             log('sequence_end: {}'.format(self.input_sequence_end), message_type='debug', title=self.farmwarename)
             log('save_meta_key: {}'.format(self.input_save_meta_key), message_type='debug', title=self.farmwarename)
             log('save_meta_value: {}'.format(self.input_save_meta_value), message_type='debug', title=self.farmwarename)
+            log('offset_x: {}'.format(self.input_offset_x), message_type='debug', title=self.farmwarename)
+            log('offset_y: {}'.format(self.input_offset_y), message_type='debug', title=self.farmwarename)
             log('default_z: {}'.format(self.input_default_z), message_type='debug', title=self.farmwarename)
             log('default_speed: {}'.format(self.input_default_speed), message_type='debug', title=self.farmwarename)
             log('debug: {}'.format(self.input_debug), message_type='debug', title=self.farmwarename)
@@ -68,7 +80,7 @@ class MyFarmware():
             log("{} -> {}".format(status_code,text), message_type='error', title=self.farmwarename + ' check_celerypy')
             raise
 
-    def apply_filters(self, points, point_name='', openfarm_slug='', age_min_day=0, age_max_day=36500, meta_key='', meta_value='', pointer_type='Plant'):
+    def apply_filters(self, points, point_name='', openfarm_slug='', age_min_day=0, age_max_day=36500, meta_key='', meta_value='', min_x='none', max_x='none', min_y='none', max_y='none', pointer_type='Plant'):
         if self.input_debug >= 1: log(points, message_type='debug', title=str(self.farmwarename) + ' : load_points')
         filtered_points = []
         now = datetime.datetime.utcnow()
@@ -92,17 +104,28 @@ class MyFarmware():
                             b_meta = ((p['meta'][meta_key]).lower() != meta_value.lower())
                         elif self.input_filter_meta_op.lower() == "regex":
                             b_meta = bool(re.compile(meta_value).match(p['meta'][meta_key]))
-                        elif self.input_filter_meta_op.lower() == "days":
+                        elif self.input_filter_meta_op.lower() == "daysmax":
                             b_meta = bool((datetime.datetime.utcnow() - datetime.datetime.strptime(p['meta'][meta_key], '%Y-%m-%dT%H:%M:%S.%fZ')).days <= meta_value)
-                        elif self.input_filter_meta_op.lower() == "minutes":
-                            b_meta = bool((datetime.datetime.utcnow() - datetime.datetime.strptime(p['meta'][meta_key], '%Y-%m-%dT%H:%M:%S.%fZ')).minute <= meta_value)     
+                        elif self.input_filter_meta_op.lower() == "minutesmax":
+                            b_meta = bool((datetime.datetime.utcnow() - datetime.datetime.strptime(p['meta'][meta_key], '%Y-%m-%dT%H:%M:%S.%fZ')).minute <= meta_value)
+                        elif self.input_filter_meta_op.lower() == "daysmin":
+                            b_meta = bool((datetime.datetime.utcnow() - datetime.datetime.strptime(p['meta'][meta_key], '%Y-%m-%dT%H:%M:%S.%fZ')).days >= meta_value)
+                        elif self.input_filter_meta_op.lower() == "minutesmin":
+                            b_meta = bool((datetime.datetime.utcnow() - datetime.datetime.strptime(p['meta'][meta_key], '%Y-%m-%dT%H:%M:%S.%fZ')).minute >= meta_value)     
                         else:
                             b_meta = False
                     except Exception as e:
                         b_meta = False
                 else:
                     b_meta = True
-                if  (p['name'].lower() == point_name.lower() or point_name == '*') and (p['openfarm_slug'].lower() == openfarm_slug.lower() or openfarm_slug == '*') and (age_min_day <= age_day <= age_max_day) and b_meta==True:
+                if str(min_x).lower() == 'none' or str(min_y).lower() == 'none' or str(max_x).lower() == 'none' or str(max_y).lower() == 'none':
+                    b_coordinate = True
+                else:
+                    if int(min_x) <= int(p['x']) <= int(max_x) and int(min_y) <= int(p['y']) <= int(max_y):
+                        b_coordinate = True
+                    else:
+                        b_coordinate = False
+                if  (p['name'].lower() == point_name.lower() or point_name == '*') and (p['openfarm_slug'].lower() == openfarm_slug.lower() or openfarm_slug == '*') and (age_min_day <= age_day <= age_max_day) and b_meta==True and b_coordinate:
                     filtered_points.append(p.copy())
         return filtered_points
 
@@ -115,6 +138,10 @@ class MyFarmware():
             age_max_day=self.input_age_max_day,
             meta_key=self.input_filter_meta_key,
             meta_value=self.input_filter_meta_value,
+            min_x=self.input_filter_min_x,
+            min_y=self.input_filter_min_y,
+            max_x=self.input_filter_max_x,
+            max_y=self.input_filter_max_y,
             pointer_type='Plant')
         if self.input_debug >= 1: log(self.points, message_type='debug', title=str(self.farmwarename) + ' : load_points_with_filters')
         
@@ -184,7 +211,7 @@ class MyFarmware():
             if self.input_debug < 2: 
                 self.check_celerypy(move_absolute(
                     location=[point['x'],point['y'] ,self.input_default_z],
-                    offset=[0, 0, 0],
+                    offset=[self.input_offset_x, self.input_offset_y, 0],
                     speed=self.input_default_speed))
 
     def save_meta(self,point):
